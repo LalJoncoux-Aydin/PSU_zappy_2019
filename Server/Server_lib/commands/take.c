feea1ce685a87d0fadb0c15dd6ad934ae70cc254
lal.joncoux-aydin <lal.joncoux-aydin@epitech.eu>
2020-06-21 23:20:23 +0200

    [FIX] Take working

--- Server/Server_lib/commands/take.c
+++ Server/Server_lib/commands/take.c
@@ -15,52 +15,60 @@ void take(int fd_cli, client_t *cli, server_t *server, char *command)
     type_obj = str_breaker(command, ' ', 2, 0);
     if (type_obj == NULL)
         error("Error : Malloc fail");
-    if (strncmp(type_obj, "food\n", 5) == 0) {
+    printf("TYPE OBJ = %s\n", type_obj);
+    if (strncmp(type_obj, "food\n", 4) == 0) {
         if (ia_cell->q0 > 0) {
             send(fd_cli, "ok\n", 3, 0);
             ia_cell->q0 -= 1;
+            cli->ai->invent->q0 += 1;
         } else
             send(fd_cli, "ko\n", 3, 0);
     }
-    if (strncmp(type_obj, "linemate\n", 9) == 0) {
+    if (strncmp(type_obj, "linemate\n", 8) == 0) {
         if (ia_cell->q1 > 0) {
             send(fd_cli, "ok\n", 3, 0);
             ia_cell->q1 -= 1;
+            cli->ai->invent->q1 += 1;
         } else
             send(fd_cli, "ko\n", 3, 0);
     }
-    if (strncmp(type_obj, "deraumere\n", 10) == 0) {
+    if (strncmp(type_obj, "deraumere\n", 9) == 0) {
         if (ia_cell->q2 > 0) {
             send(fd_cli, "ok\n", 3, 0);
             ia_cell->q2 -= 1;
+            cli->ai->invent->q2 += 1;
         } else
             send(fd_cli, "ko\n", 3, 0);
     }
-    if (strncmp(type_obj, "sibur\n", 6) == 0) {
+    if (strncmp(type_obj, "sibur\n", 5) == 0) {
         if (ia_cell->q3 > 0) {
             send(fd_cli, "ok\n", 3, 0);
             ia_cell->q3 -= 1;
+            cli->ai->invent->q3 += 1;
         } else
             send(fd_cli, "ko\n", 3, 0);
     }
-    if (strncmp(type_obj, "mendiane\n", 9) == 0) {
+    if (strncmp(type_obj, "mendiane\n", 8) == 0) {
         if (ia_cell->q4 > 0) {
             send(fd_cli, "ok\n", 3, 0);
             ia_cell->q4 -= 1;
+            cli->ai->invent->q4 += 1;
         } else
             send(fd_cli, "ko\n", 3, 0);
     }
-    if (strncmp(type_obj, "phiras\n", 7) == 0) {
+    if (strncmp(type_obj, "phiras\n", 6) == 0) {
         if (ia_cell->q5 > 0) {
             send(fd_cli, "ok\n", 3, 0);
             ia_cell->q5 -= 1;
+            cli->ai->invent->q5 += 1;
         } else
             send(fd_cli, "ko\n", 3, 0);
     }
-    if (strncmp(type_obj, "thystame\n", 9) == 0) {
+    if (strncmp(type_obj, "thystame\n", 8) == 0) {
         if (ia_cell->q6 > 0) {
             send(fd_cli, "ok\n", 3, 0);
             ia_cell->q6 -= 1;
+            cli->ai->invent->q6 += 1;
         } else
             send(fd_cli, "ko\n", 3, 0);
     }
