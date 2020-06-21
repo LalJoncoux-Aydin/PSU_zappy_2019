0cd0f5e353d189bd3228abb0a053c2cbbaa8ebcf
Needoliprane <louis-auguste.dumas@epitech.eu>
2020-06-21 23:47:54 +0200

    [update] elevation mostly working

--- Server/Server_lib/commands/elevation.c
+++ Server/Server_lib/commands/elevation.c
@@ -7,18 +7,16 @@
 
 #include "command.h"
 
-//  FD CLI -> fd du client qui te parle
-//  CLIENT -> client qui te parle (définit dans include/struct_server.h)
-//  SERVER -> server (definit dans include/struct_server.h)
-//  COMMAND -> ton message (regarde dans /tools il y a des outils pour le manipuler)
 void elevation(int fd_cli, client_t *clis, server_t *server, char *command)
 {
     char *buff = NULL;
 
+    fd_cli = fd_cli;
+    clis = clis;
+    server = server;
+    command = command;
     buff = malloc(sizeof(char) * MESSAGE_SIZE);
-    if (buff == NULL)
-        error("Error : malloc failed\n");
-	// TODO : Remplir le buffer
+    strcpy(buff, "Elevation underway\0");
     send(fd_cli, buff, strlen(buff), 0);
     free(buff);
 }
