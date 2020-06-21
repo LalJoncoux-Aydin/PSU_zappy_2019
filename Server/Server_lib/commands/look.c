ce7a55380530c577889ac5c78ad1731f7494c10f
lal.joncoux-aydin <lal.joncoux-aydin@epitech.eu>
2020-06-21 19:52:40 +0200

    [ADD] Look protocole correct msg

--- Server/Server_lib/commands/look.c
+++ Server/Server_lib/commands/look.c
@@ -7,64 +7,56 @@
 
 #include "command.h"
 
-static void writing_element(tile_t *cell, int fd_cli)
+static char *writing_element(tile_t *cell, char *send_look)
 {
-    char send_look[MESSAGE_SIZE * 3] = "[" "player";
+    strcat(send_look, "player");
     if (cell->q0 > 0) {
-        strcat(send_look, ",");
         for (int i = cell->q0; i > 0; i--)
             strcat(send_look, " food");
-    } else {
-        strcat(send_look, ",");
     }
     if (cell->q1 > 0) {
-        strcat(send_look, ",");
         for (int i = cell->q1; i > 0; i--)
             strcat(send_look, " linemate");
-    } else {
-        strcat(send_look, ",");
     }
     if (cell->q2 > 0) {
-        strcat(send_look, ",");
         for (int i = cell->q2; i > 0; i--)
             strcat(send_look, " deraumere");
-    } else {
-        strcat(send_look, ",");
     }
     if (cell->q3 > 0) {
-        strcat(send_look, ",");
         for (int i = cell->q3; i > 0; i--)
             strcat(send_look, " sibur");
-    } else {
-        strcat(send_look, ",");
     }
     if (cell->q4 > 0) {
-        strcat(send_look, ",");
         for (int i = cell->q4; i > 0; i--)
             strcat(send_look, " mendiane");
-    } else {
-        strcat(send_look, ",");
     }
     if (cell->q5 > 0) {
-        strcat(send_look, ",");
         for (int i = cell->q5; i > 0; i--)
             strcat(send_look, " phiras");
-    } else {
-        strcat(send_look, ",");
     }
     if (cell->q6 > 0) {
-        strcat(send_look, ",");
         for (int i = cell->q6; i > 0; i--)
             strcat(send_look, " thystame");
-    } else {
-        strcat(send_look, ",");
     }
-    strcat(send_look, "]\n\0");
-    send(fd_cli, send_look, strlen(send_look), 0);
+    return send_look;
 }
 
 void look(int fd_cli, client_t *cli, server_t *server,
 __attribute__((unused)) char *command)
 {
-    writing_element(&server->map[cli->ai->y][cli->ai->x], fd_cli);
+    char *buff = NULL;
+
+    buff = malloc(sizeof(char) * MESSAGE_SIZE * MESSAGE_SIZE);
+    if (buff == NULL)
+        error("Error : malloc fail\n");
+    strcat(buff, "[");
+    for (int i = 0; i < 15; i++) {
+        buff = writing_element(&server->map[cli->ai->y][cli->ai->x], buff);
+        if (i != 14)
+            strcat(buff, ", ");
+    }
+    strcat(buff, "]\n");
+    printf("buff = %s\n", buff);
+    send(fd_cli, buff, strlen(buff), 0);
+    free(buff);
 }
